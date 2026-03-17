deb5317e6627e3134ed01e030257971fd60691d9
emptysoal <57931586+emptysoal@users.noreply.github.com>
2026-03-17 11:09:07 +0800

    Add files via upload

---
+++ 10_sub_query_with_classify/README.md
@@ -0,0 +1,28 @@
+# RAG：创建子查询
+
+把用户提问分解为子查询，并对子查询分类，不同的子查询去检索其对应的知识库，最后汇总。
+
+- graph
+
+```mermaid
+graph TD;
+	__start__([<p>__start__</p>]):::first
+	classify(classify)
+	retriever_01(retriever_01)
+	retriever_02(retriever_02)
+	synthesize(synthesize)
+	__end__([<p>__end__</p>]):::last
+	retriever_01 --> synthesize;
+	retriever_02 --> synthesize;
+	__start__ --> classify;
+	classify -.-> retriever_01;
+	classify -.-> retriever_02;
+	synthesize --> __end__;
+	classDef default fill:#f2f0ff,line-height:1.2
+	classDef first fill-opacity:0
+	classDef last fill:#bfb6fc
+```
+
+- 图片来源：`Milvus` 官方网站
+
+![](https://milvus.io/docs/v2.6.x/assets/advanced_rag/sub_query.png)
---
+++ 10_sub_query_with_classify/rag.py
@@ -0,0 +1,205 @@
+"""
+
+"""
+
+import operator
+from typing import Annotated, Literal, TypedDict
+from pydantic import BaseModel, Field
+
+from langchain.messages import SystemMessage, HumanMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.types import Send
+from langchain_openai import ChatOpenAI
+
+from retriever import MilvusRetriever
+from config import *
+
+cnn_milvus_retriever = MilvusRetriever(milvus_collection=CNN_COLLECTION_NAME)  # 检索 CNN 相关的文档
+llm_milvus_retriever = MilvusRetriever(milvus_collection=LM_COLLECTION_NAME)  # 检索 LLM 相关的文档
+
+# 这里的分解子查询和回复的大模型使用的是同一个，也可以使用不同的
+sub_query_llm = ChatOpenAI(
+    model_name=LLM_MODEL_NAME,
+    openai_api_base=LLM_ENDPOINT,
+    openai_api_key=LLM_API_KEY,
+    temperature=LLM_TEMPERATURE
+)
+
+llm = ChatOpenAI(
+    model_name=LLM_MODEL_NAME,
+    openai_api_base=LLM_ENDPOINT,
+    openai_api_key=LLM_API_KEY,
+    temperature=LLM_TEMPERATURE
+)
+
+
+# 定义状态
+class AgentInput(TypedDict):
+    """每个子查询的简单输入状态。"""
+    query: str
+
+
+class AgentOutput(TypedDict):
+    """单个子查询的输出"""
+    category: str
+    result: str
+
+
+class Classification(TypedDict):
+    """分解后的某个子查询，和它对应的类别（属于哪个垂直领域）"""
+    # category: Literal["CNN(Convolutional Neural Network)", "LLM(Large Language Model)"]
+    category: Literal["CNN", "LLM"]
+    query: str
+
+
+class RouterState(TypedDict):
+    """路由节点的状态"""
+    query: str
+    classifications: list[Classification]
+    results: Annotated[list[AgentOutput], operator.add]  # Reducer collects parallel results
+    final_answer: str
+
+
+# 构建路由器工作流程
+# 为分类器定义结构化的输出模式
+class ClassificationResult(BaseModel):
+    classifications: list[Classification] = Field(description="存放查询分解后得到的子查询的信息。")
+
+
+def classify_query(state: RouterState) -> dict:
+    """对查询进行分解，得到子查询以及对应的类别"""
+    structured_llm = sub_query_llm.with_structured_output(ClassificationResult)
+
+    classification_result = structured_llm.invoke(
+        [
+            SystemMessage(content=SUB_QUERY_SYSTEM_PROMPT),
+            HumanMessage(content=state["query"])
+        ]
+    )
+
+    return {"classifications": classification_result.classifications}
+
+
+# 测试查询分解
+# test_query = {"query": "卷积神经网络的原理是什么？有哪些应用场景？"}
+# ret = classify_query(test_query)
+# print(ret)
+
+
+def route_to_retrievers(state: RouterState) -> list[Send]:
+    """根据子查询及分类向各节点分发。"""
+    return [
+        Send(c["category"], {"query": c["query"]}) for c in state['classifications']
+    ]
+
+
+def retrieve_cnn_docs(state: AgentInput) -> dict:
+    """根据用户的查询，从 CNN 向量数据库检索相关的文档"""
+
+    # 检索
+    doc_chunks = cnn_milvus_retriever.retrieve(state["query"])
+    files = [doc_chunk_dict["file_name"] for doc_chunk_dict in doc_chunks]
+    docs = [doc_chunk_dict["text"] for doc_chunk_dict in doc_chunks]
+
+    context_list = []
+    for file_name, doc_content in zip(files, docs):
+        one_context = f"文档名：\n{file_name}\n文档内容：\n{doc_content}"
+        context_list.append(one_context)
+    context = "\n\n".join(context_list)
+
+    return {"results": [{"category": "CNN", "result": context}]}
+
+
+def retrieve_llm_docs(state: AgentInput) -> dict:
+    """根据用户的查询，从 LLM 向量数据库检索相关的文档"""
+
+    # 检索
+    doc_chunks = llm_milvus_retriever.retrieve(state["query"])
+    files = [doc_chunk_dict["file_name"] for doc_chunk_dict in doc_chunks]
+    docs = [doc_chunk_dict["text"] for doc_chunk_dict in doc_chunks]
+
+    context_list = []
+    for file_name, doc_content in zip(files, docs):
+        one_context = f"文档名：\n{file_name}\n文档内容：\n{doc_content}"
+        context_list.append(one_context)
+    context = "\n\n".join(context_list)
+
+    return {"results": [{"category": "LLM", "result": context}]}
+
+
+def synthesize_results(state: RouterState) -> dict:
+    """将所有子查询节点的结果组合成一个连贯的答案。"""
+    if not state["results"]:
+        return {"final_answer": "没有从任何知识来源中找到结果。"}
+
+    # Format results for synthesis
+    formatted = [
+        f"{r['category']} 相关的检索结果为:\n{r['result']}" for r in state["results"]
+    ]
+    context = "\n\n".join(formatted)
+
+    system_message = SYSTEM_PROMPT.format(context_info=context)
+
+    synthesis_response = llm.invoke(
+        [
+            SystemMessage(content=system_message),
+            HumanMessage(content=state["query"])
+        ]
+    )
+
+    return {"final_answer": synthesis_response.content}
+
+
+def build_workflow():
+    # Build workflow
+    workflow = StateGraph(RouterState)
+
+    # Add nodes
+    workflow.add_node("classify", classify_query)
+    workflow.add_node("CNN", retrieve_cnn_docs)
+    workflow.add_node("LLM", retrieve_llm_docs)
+    workflow.add_node("synthesize", synthesize_results)
+
+    # Add edges to connect nodes
+    workflow.add_edge(START, "classify")
+    workflow.add_conditional_edges(
+        "classify",
+        route_to_retrievers,
+        ["CNN", "LLM"]
+    )
+    workflow.add_edge("CNN", "synthesize")
+    workflow.add_edge("LLM", "synthesize")
+    workflow.add_edge("synthesize", END)
+
+    # Compile the agent
+    rag = workflow.compile()
+
+    # Show the workflow
+    # graph = rag.get_graph(xray=True)
+    # mermaid_code = graph.draw_mermaid()
+    # print(mermaid_code)
+
+    return rag
+
+
+if __name__ == '__main__':
+    rag_workflow = build_workflow()
+
+    # input_ = {"query": "卷积神经网络的应用场景有哪些？"}
+    # input_ = {"query": "大模型的应用场景有哪些？"}
+    input_ = {"query": "卷积神经网络和大模型的应用场景分别有哪些？"}
+    # result = rag_workflow.invoke(input_)
+    # print(result)
+
+    for stream_mode, data in rag_workflow.stream(
+            input_,
+            stream_mode=["updates", "messages"]
+    ):
+        if stream_mode == "updates":
+            for source, update in data.items():
+                print("=== Node Name:", source)
+                print("=== Updated State:", update)
+        if stream_mode == "messages":
+            msg, metadata = data
+            if msg.content and metadata["langgraph_node"] == "synthesize":
+                print(msg.content, end="", flush=True)
